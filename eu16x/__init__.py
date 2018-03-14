db67438fe005c8463135be85de1dfdf43b89c9fb
Sean Harrison <sah@bookgenesis.com>
2018-03-14 12:42:31 -0500

    csv_to_data(): + Add unescape=True and return_keys=False as parameters, retaining the same defaults but providing additional options. 	If unescape=False, don't unescape \\t, \\r, \\n 	If return_keys=True, return data, keys rather than just data. + Add logging for the normalize_text() and csv_to_data() so that users can see progress on long-running conversion.

--- eu16x/__init__.py
+++ eu16x/__init__.py
@@ -3,6 +3,7 @@ import os, re, sys, json, logging
 from lxml.etree import Entity
 from bl.string import String
 from bl.text import Text
+from bl.json import JSON
 from bxml import XML
 from bxml.builder import Builder
 
@@ -14,7 +15,7 @@ NS = {
 }
 AID_PSTYLE_KEY = "{%(aid)s}pstyle" % NS
 
-def csv_to_data(fn, encoding='UTF-16', delimiter='\t', quote='"', headers=True, omit_empty=True):
+def csv_to_data(fn, encoding='UTF-16', delimiter='\t', quote='"', headers=True, omit_empty=True, unescape=True, return_keys=False):
     t = Text(fn=fn, encoding=encoding)
     text = normalize_text(t.text, quote=quote)
     lines = text.split('\n')
@@ -23,15 +24,23 @@ def csv_to_data(fn, encoding='UTF-16', delimiter='\t', quote='"', headers=True, 
     else:
         keys = [excel_key(i) for i in range(len(lines[0].split(delimiter)))]
     data = []
+    log.debug("%d lines of data" % len(lens))
     for line in lines:
+        if lines.index(line) % 1000 == 0: log.debug("%d lines processed" % lines.index(line))
         item = {}
-        values = [v.replace('\\t', '\t').replace('\\n', '\n').replace('\\r', '\n') for v in line.split(delimiter)]
+        if unescape==True:
+            values = [v.replace('\\t', '\t').replace('\\n', '\n').replace('\\r', '\n') for v in line.split(delimiter)]
+        else:
+            values = line.split(delimiter)
         for i in range(len(values)):
             if values[i].strip() != '' or omit_empty==False:        # omit empty elements
                 item[keys[i]] = values[i].strip()
         if len(item.keys()) > 0:
             data.append(item)
-    return data
+    if return_keys==True:
+        return data, keys
+    else:
+        return data
 
 def csv_to_xml(fn, encoding='UTF-16', delimiter='\t', quote='"', headers=True, tag='item', namespace=None, aid=False, omit_empty=True):
     t = Text(fn=fn, encoding=encoding)
@@ -49,32 +58,28 @@ def normalize_text(text, quote='"', escape='\\'):
     quoted = False
     i = 0
     n = len(text)
+    log.debug('normalize: %d characters of text' % n)
     while i < n:
+        if i > 0 and i % 100000 == 0: log.debug('  %d%% normalized' % int(100*i/n))
         c = text[i]
-        # log.debug("%d %c" % (i, c))
         if c in [escape, quote] and i < n-1 and text[i+1]==quote:
-            # log.debug('escaped quote,', text[i+1])
             text = text[:i] + text[i+1:]
             # i += 1
         elif c=='\r':
-            # log.debug('linebreak')
             text = text[:i] + '\\r' + text[i+1:]
             n = len(text)
             i -= 1
         elif c==quote:
             quoted = not(quoted)
-            # log.debug('quote, quoted = %r' % quoted)
             text = text[:i] + text[i+1:]
             n = len(text)
             i -=1 
         elif quoted==True:
             if c=='\n':
-                # log.debug('newline')
                 text = text[:i] + '\\n' + text[i+1:]
                 n = len(text)
                 i += 1
             elif c=='\t':
-                # log.debug('tab')
                 text = text[:i] + '\\t' + text[i+1:]
                 n = len(text)
                 i += 1
